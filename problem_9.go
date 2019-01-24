4cee0aa01f52a3eeb7cdcb135b5fa4f51ba178bc
Hamed Momeni <hamed.ma7@gmail.com>
2019-01-24 12:28:50 +0330

    problem 11

--- problem_9.go
+++ problem_9.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	for i := 1.0; i <= 10000; i++ {
-		for j := 1.0; j <= 10000; j++ {
+	for i := 1.0; i <= 1000; i++ {
+		for j := 1.0; j <= 1000; j++ {
 			sum := i + j + math.Sqrt(float64((i*i)+(j*j)))
 			if sum == 1000 {
 				fmt.Printf("%f", i*j*math.Sqrt(float64((i*i)+(j*j))))
