90f4491c2d67d9f3995a21e11317ec7abb27fb96
Hamed Momeni <hamed.ma7@gmail.com>
2019-03-21 22:12:26 +0330

    added BFS

--- dfs_bfs.go
+++ dfs_bfs.go
@@ -3,6 +3,7 @@ package main
 import (
 	"math/rand"
 	"fmt"
+	"time"
 )
 
 type Node struct {
@@ -12,6 +13,9 @@ type Node struct {
 }
 
 func (n *Node) print() string {
+	if n == nil {
+		return "node is nil"
+	}
 	var l = -10000
 	if n.left != nil {
 		l = n.left.value
@@ -40,22 +44,44 @@ func searchDFS(root *Node, target int) *Node {
 	return nil
 }
 
+func searchBFS(root *Node, target int) *Node {
+	if root.value == target {
+		return root
+	}
+	if root.left != nil && root.left.value == target {
+		return root.left
+	}
+
+	if root.right != nil && root.right.value == target {
+		return root.right
+	}
+
+	if root.left != nil {
+		return searchBFS(root.left, target)
+	}
+	if root.right != nil {
+		return searchBFS(root.right, target)
+	}
+	return nil
+}
+
 func main() {
+	rand.Seed(time.Now().Unix())
 	root := new(Node)
 	tRoot := root
-	for i := 0; i < 10; i++ {
-		val := rand.Intn(1000)
+	for i := 0; i < 100000; i++ {
+		val := rand.Intn(100000)
 		tRoot.value = val
 		if val%2 == 0 {
 			tRoot.left = new(Node)
 			tRoot = tRoot.left
+		} else if val%9 == 0 {
+			val = 55
 		} else {
 			tRoot.right = new(Node)
 			tRoot = tRoot.right
 		}
 	}
-	tRoot.right = new(Node)
-	tRoot.right.value = 55
 
-	fmt.Println(searchDFS(root, 55).print())
+	fmt.Println(searchBFS(root, 55).print())
 }
