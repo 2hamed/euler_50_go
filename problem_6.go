d0acec6cb8a7d38a14c9ddb3025c56d2478ce5ff
Hamed Momeni <hamed.ma7@gmail.com>
2019-04-01 20:33:32 +0430

    fixing typo

--- problem_6.go
+++ problem_6.go
@@ -6,14 +6,14 @@ import (
 )
 
 func main() {
-	var sqauredSum int64
+	var squaredSum int64
 	var sumSquared int64
 
 	for i := int64(1); i <= 100; i++ {
-		sqauredSum += i * i
+		squaredSum += i * i
 		sumSquared += i
 	}
 	sumSquared *= sumSquared
 
-	fmt.Println("Diff: ", int64(math.Abs(float64(sumSquared-sqauredSum))))
+	fmt.Println("Diff: ", int64(math.Abs(float64(sumSquared-squaredSum))))
 }
