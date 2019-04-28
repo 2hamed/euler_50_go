f235565e54b587303fbdbf6ba58f2da21fdbe6a9
Hamed Momeni <hamed.ma7@gmail.com>
2019-04-28 10:39:22 +0430

    coins dp

--- coins.go
+++ coins.go
@@ -1,26 +1,33 @@
 package main
 
 import (
-	"math"
 	"fmt"
 )
 
-func getWays(n int64, c []int64) int64 {
-	return coins_dp(c, len(c)-1, n)
+var memo = make(map[int64]map[int]int64)
+
+func getWays(cap int64, c []int64) int64 {
+	return getWaysDP(cap, c, len(c))
+
 }
 
-func coins_dp(coins []int64, n int, cap int64) int64 {
-	var result int64
-	if n < 0 || coins[n] > cap {
+func getWaysDP(capRemaining int64, coins []int64, coinNum int) int64 {
+	if capRemaining == 0 {
+		return 1
+	} else if capRemaining < 0 || coinNum <= 0 {
 		return 0
-	} else {
-		t1 := coins[n] + coins_dp(coins, n-1, cap-coins[n])
-		t2 := coins_dp(coins, n-1, cap)
-		result = int64(math.Max(float64(t1), float64(t2)))
+	} else if r, ok := memo[capRemaining][coinNum]; ok {
+		return r
 	}
 
-	return result
+	r := getWaysDP(capRemaining, coins, coinNum-1) + getWaysDP(capRemaining-coins[coinNum-1], coins, coinNum)
+	if memo[capRemaining] == nil {
+		memo[capRemaining] = make(map[int]int64)
+	}
+	memo[capRemaining][coinNum] = r
+	return r
 }
+
 func main() {
-	fmt.Println(getWays(10, []int64{1,9}))
+	fmt.Println(getWays(10, []int64{2, 5, 3, 6}))
 }
